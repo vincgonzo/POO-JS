60421530458cb4849914e6aaadd59b65b194bae3
vincent-castanie <vincgonzo@hotmail.fr>
2018-01-23 09:51:22 +0100

    prepare activity base

--- contactGestion/js/contacts.js
+++ contactGestion/js/contacts.js
@@ -3,9 +3,31 @@ Activité : gestion des contacts
 */
 
 var Contacts = {
-  init: function(){
+  init: function(nom, prenom){
     this.nom = nom;
-    this.prenom = sante;
+    this.prenom = prenom;
   }
 }
 var arrayContacts = [];
+
+
+var Contact = Object.create(Contacts);
+Contact.initContact = function(nom, prenom){
+  this.init(nom, prenom);
+}
+
+var jp = Object.create(Contact);
+jp.initContact('Vandale', 'Jean pierre');
+
+var elsa = Object.create(Contact);
+elsa.initContact('Maigne', 'Elsa');
+
+var erik = Object.create(Contact);
+erik.initContact('Vandersmith', 'Erik');
+
+arrayContacts.push(jp, elsa, erik);
+
+
+arrayContacts.forEach(function(Contacts){
+  console.log(Contacts.nom +' est son nom et '+ Contacts.prenom + ' son prénom, bien sûr.');
+});
