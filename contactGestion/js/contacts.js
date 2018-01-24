b3395e87b9b068325dad0ecd6d28f726e5c4bb31
vincent-castanie <vincgonzo@hotmail.fr>
2018-01-24 08:35:23 +0100

    activity 2

--- contactGestion/js/contacts.js
+++ contactGestion/js/contacts.js
@@ -1,7 +1,6 @@
 /*
 Activité : gestion des contacts
 */
-
 var Contacts = {
   init: function(nom, prenom){
     this.nom = nom;
@@ -9,7 +8,6 @@ var Contacts = {
   }
 }
 var arrayContacts = [];
-
 
 var Contact = Object.create(Contacts);
 Contact.initContact = function(nom, prenom){
@@ -28,6 +26,40 @@ erik.initContact('Vandersmith', 'Erik');
 arrayContacts.push(jp, elsa, erik);
 
 
-arrayContacts.forEach(function(Contacts){
-  console.log(Contacts.nom +' est son nom et '+ Contacts.prenom + ' son prénom, bien sûr.');
-});
+function logArray(){
+  var html = '';
+  arrayContacts.forEach(function(Contacts, index){
+    html += '<li>Le contact numéro '+ index +' est ' + Contacts.nom +' '+ Contacts.prenom + '.</li>';
+    index++;
+  });
+
+  return html;
+}
+
+function callContact(){
+  var listDiv = document.getElementById("listContact");
+
+  var listContact = logArray();
+  listDiv.innerHTML = listContact;
+}
+
+function addContact(){
+  var nom = prompt("Entrez le Nom de la personne");
+  var prenom = prompt("Entrez le Prénom de la personne");
+
+  var newContact = Object.create(Contact);
+  newContact.initContact(nom, prenom);
+  arrayContacts.push(newContact);
+
+  flashBag(newContact);
+}
+
+function flashBag(contact){
+  var flashMessage = document.getElementById("flashBag");
+
+  flashMessage.innerHTML = '<div class="alert alert-success" role="alert"><strong>Bravo</strong> vous avez bien créer votre nouveau contact.<br/>Son nom est '+contact.nom+' et son prénom '+contact.prenom+'<br/>Réappuyez sur list des contacts pour mettre à jour celle-ci.</div>';
+
+  setTimeout(function(){
+    flashMessage.innerHTML = "";
+  }, 3000);
+}
